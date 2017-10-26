dac6435883c86f61950d79ce73434f5651c1f380
Stephanie Wong <swongwrites@gmail.com>
2017-10-25 17:28:58 -0700

    Create data generation script

--- schema.sql
+++ schema.sql
@@ -6,5 +6,23 @@ USE stocky;
 
 CREATE TABLE users (
   id int NOT NULL AUTO_INCREMENT,
-  PRIMARY KEY (id)
+  PRIMARY KEY (ID)
+);
+
+CREATE TABLE indicators (
+    id int NOT NULL AUTO_INCREMENT,
+    user_id int NOT NULL,
+    indicator varchar(100) NOT NULL,
+    totalViews int NOT NULL, 
+    average int NOT NULL, 
+    PRIMARY KEY (ID)
+
+);
+
+CREATE TABLE profits (
+    id int NOT NULL AUTO_INCREMENT,
+    user_id int NOT NULL,
+    currencyPair varchar(100) NOT NULL,
+    profitNumber int NOT NULL,
+    PRIMARY KEY (ID)
 );
---
+++ server/dataGenerator.js
@@ -0,0 +1,63 @@
+var mocker = require('mocker-data-generator').default;
+const database = require('../database/index.js');
+var faker = require('faker');
+
+
+const shortid = require('shortid');
+
+const indicator = [
+    'macd',
+    'ema',
+    'movingAvg'
+];
+
+const majorPair = [
+    'EURUSD',
+    'GBPUSD',
+    'USDCAD',
+    'USDCHF',
+    'USDJPY',
+    'EURGBP',
+    'EURCHF',
+    'AUDUSD',
+    'EURJPY',
+    'GBPJPY',
+]
+
+var myNamespace = {};
+
+myNamespace.round = function(number, precision) {
+    var factor = Math.pow(10, precision);
+    var tempNumber = number * factor;
+    var roundedTempNumber = Math.round(tempNumber);
+    return roundedTempNumber / factor;
+};
+
+var userid =  faker.random.number();
+var views = faker.random.number(50);
+var sessions = faker.random.number({min:50, max:100});
+var average = views/sessions;
+
+var user = {
+    id: userid,
+    totalSessions: sessions,
+};
+
+var indicators = {
+    user_id: userid,
+    indicator: indicator[Math.floor(Math.random() * indicator.length)],
+    totalViews: views,
+    average: myNamespace.round(average, 2),
+};
+
+var profits = {
+    user_id: userid,
+    currencyPair: majorPair[Math.floor(Math.random() * majorPair.length)],
+    profitNumber: faker.random.number({min:10, max:10000000000000}),
+};
+
+
+console.log(user);
+console.log(indicators)
+console.log(profits);
+
